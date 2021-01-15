bd761af9f2d7760bac3157c3f4ba409ab6305ef5
elarouche-telpos <eric.larouche@telops.com>
2021-01-15 20:54:15 +0000

    implémentation du code de ntxdetect
    git-svn-id: http://einstein/svn/ircsoft/ircsoft/trunk/TSIRToolbox@11008 41d30f82-c98b-d346-91cf-d5a36744e506

---
+++ src/NTXDetect.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <PvVersion.h>
+#include <PvDevice.h>
+#include <PvDeviceFinderWnd.h>
+#include "BuildInfo.h"
+#include "TSIRToolbox.h"
+#include "FileManager.h"
+
+using namespace std;
+
+#define EXIT_PBXMX1 0
+#define EXIT_PBXMX4 1
+#define EXIT_FAILURE 255
+
+int main()
+{
+	PvResult result;
+	FileManager fm;
+	PvGenParameterArray *deviceParams;
+	int64_t IPEngineDeviceID;
+	int retval;
+
+	// Get device parameters
+	if (fm.Open() != IRC_SUCCESS)
+	{
+		cerr << "Unable to open port." << endl;
+		return EXIT_FAILURE;
+	}
+	deviceParams = fm.GetGenParams();
+
+	// Detect NTX-Mini platform
+	deviceParams->GetIntegerValue("IPEngineDeviceID", IPEngineDeviceID);
+	if (IPEngineDeviceID == 20) retval = EXIT_PBXMX1;
+	if (IPEngineDeviceID == 34) retval = EXIT_PBXMX4;
+	else retval = EXIT_FAILURE;
+
+	// Return exit code to OS
+	return retval;
+}
